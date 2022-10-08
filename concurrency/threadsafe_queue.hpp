379095370c3bb7f64c12a70f962006c4dc483746
gwongsam <gwongsam@gmail.com>
2022-10-08 13:29:58 +0800

    feat Modify Thread Safe Queue with Dummy Node

--- concurrency/threadsafe_queue.hpp
+++ concurrency/threadsafe_queue.hpp
@@ -66,7 +66,7 @@ template <typename T>
 class queue {
  private:
   struct node {
-    T data;
+    std::shared_ptr<T> data;
     std::unique_ptr<node> next;
     node(T data_) : data(std::move(data_)) {}
   };
@@ -74,31 +74,26 @@ class queue {
   node *tail;
 
  public:
-  queue() : tail(nullptr) {}
+  queue() : head(new node), tail(head.get()) {}
   queue(const &queue other) = delete;
   queue &operator=(const queue &other) = delete;
 
   std::shared_ptr<T> try_pop() {
-    if (!head) {
+    if (head.get() == tail) {
       return shared_ptr<T>();
     }
-    std::shared_ptr<T> const res(std::make_shared<T>(std::move(head->data)));
-    std::unique_ptr<node> const old_head = std::move(head);
+    std::shared_ptr<T> const res(head->data);
+    std::unique_ptr<node> old_head = std::move(head);
     head = std::move(old_head->next);
-    if (!head) {
-      tail = nullptr;
-    }
     return res;
   }
 
   void push(T new_value) {
-    std::unique_ptr<node> p(new node(std::move(new_value)));
+    std::shared_ptr<T> new_data(std::make_shared<T>(std::move(new_value)));
+    std::unique_ptr<node> p(new node);
+    tail->data = new_data;
     node *const new_tail = p.get();
-    if (tail) {
-      tail->next = std::mvoe(p);
-    } else {
-      head = std::move(p);
-    }
+    tail->next = std::move(p);
     tail = new_tail;
   }
 };
